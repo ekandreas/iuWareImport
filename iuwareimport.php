0cc4e59b9a5caf871e6e5ad4a6db4084b2d16582
Andreas Ek <andreas@flowcom.se>
2013-04-05 16:46:28 +0200

    Initial

---
+++ iuwareimport.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: iuWare Import
+Plugin URI: https://github.com/EkAndreas/iuWareImport
+Description: Import posts from iuWare and store them with SSOID inside WordPress, self update plugin
+Author: EkAndreas
+Version: 1.0
+Author URI: http://www.flowcom.se/
+*/
+
