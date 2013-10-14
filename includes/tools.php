b803babb5bbca929f77778f6ba2e8a6a6dcb446b
Andreas Ek <andreas@flowcom.se>
2013-10-14 13:22:27 +0200

    Schedule event

--- includes/tools.php
+++ includes/tools.php
@@ -381,9 +381,7 @@ class Mentor_iuWare_Import_Tools{
 
         update_option( 'iuware_running', '' );
 
-        if ( !wp_next_scheduled( 'cron_iuware_import' ) ) {
-            wp_schedule_event( time(), 'minute', 'cron_iuware_import' );
-        }
+        wp_schedule_event( time(), 'minute', 'cron_iuware_import' );
 
         wp_die( 'iuWare Import har kört klart.', 'iuWare Import' );
 
